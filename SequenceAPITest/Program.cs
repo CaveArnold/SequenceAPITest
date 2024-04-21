ee9d726fb17790b05aab4c6573a2c8fe9599713e
CaveArnold <CaveArnold@gmail.com>
2024-04-21 19:38:07 -0400

    Add project files.

---
+++ SequenceAPITest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SequenceAPITest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
